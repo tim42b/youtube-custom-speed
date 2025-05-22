72a8007e3d97acce1963453c53dd57e71fd58e6f
Tim Brooks <timbr42@gmail.com>
2025-05-22 03:20:55 -0700

    Fix tutorial script lint

--- static/tutorial/welcome-prep.js
+++ static/tutorial/welcome-prep.js
@@ -1,22 +1,30 @@
-'use strict';
+"use strict";
+
+/* global chrome */
 
 // click listeners for buttons
-function optionsClick(e) {
-    chrome.tabs.create({
-        url: 'chrome://extensions/?options=' + chrome.runtime.id
-    });
+function optionsClick() {
+  chrome.tabs.create({
+    url: "chrome://extensions/?options=" + chrome.runtime.id,
+  });
 }
 
-function shortcutOptionsClick(e) {
-    chrome.tabs.create({
-        url: 'chrome://extensions/shortcuts'
-    });
+function shortcutOptionsClick() {
+  chrome.tabs.create({
+    url: "chrome://extensions/shortcuts",
+  });
 }
 
-function settingsClick(e) {
-    chrome.tabs.create({
-        url: 'chrome://extensions/?id=' + chrome.runtime.id
-    });
+function settingsClick() {
+  chrome.tabs.create({
+    url: "chrome://extensions/?id=" + chrome.runtime.id,
+  });
 }
-document.querySelector('#ext-options').addEventListener('click', optionsClick);
-// document.querySelector('#chrome-settings').addEventListener('click', settingsClick);
+
+document
+  .querySelector("#ext-options")
+  .addEventListener("click", optionsClick);
+// document
+//   .querySelector('#chrome-settings')
+//   .addEventListener('click', settingsClick);
+
